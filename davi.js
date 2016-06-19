69fcd921ede8fbff3c7a202fb23fc90bf5ed7129
paulsoh <yongdoree@gmail.com>
2016-06-19 13:43:39 +0900

    MVP for pie chart and histogram

--- davi.js
+++ davi.js
@@ -128,5 +128,120 @@ d3.json('new_project_full_data.json', function(data) {
           });
 
   // Mouse hover
-  slice.on("mouseover", function(d) { console.log(d) });
+  slice.on("mouseover", function(d, i) { renderHistogram(sum_money[i], i); });
+
+//function setHistogram(){
+//    // A formatter for counts.
+//    var formatCount = d3.format(",.0f");
+
+//    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+//        width = 960 - margin.left - margin.right,
+//        height = 500 - margin.top - margin.bottom;
+
+//    var x = d3.scale.linear()
+//        .domain([100000, 1000000])
+//        .range([0, width]);
+
+//    // Generate a histogram using twenty uniformly-spaced bins.
+//    var data = d3.layout.histogram().bins(x.ticks(50))
+//        (dataset);
+
+//    var y = d3.scale.linear()
+//        .domain([0, d3.max(data, function(d) { return d.y; })])
+//        .range([height, 0]);
+
+//    var xAxis = d3.svg.axis()
+//        .scale(x)
+//        .orient("bottom");
+
+//    var svg = d3.select("body").append("svg")
+//        .attr("width", width + margin.left + margin.right)
+//        .attr("height", height + margin.top + margin.bottom)
+//      .append("g")
+//        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//    var bar = svg.selectAll(".bar")
+//        .data(data)
+//      .enter().append("g")
+//        .attr("class", "bar")
+//        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+//    bar.append("rect")
+//        .attr("x", 1)
+//        .attr("width", x(100000+data[0].dx) - 1)
+//        .attr("height", function(d) { return height - y(d.y); });
+
+//    bar.append("text")
+//        .attr("dy", ".75em")
+//        .attr("y", -10)
+//        .attr("x", x(100000+data[0].dx) / 2)
+//        .attr("text-anchor", "middle")
+//        .text(function(d) { return formatCount(d.y); });
+
+//    svg.append("g")
+//        .attr("class", "x axis")
+//        .attr("transform", "translate(0," + height + ")")
+//        .call(xAxis);
+//}
+
+  // histogram_data set up
+  var svg = d3.select("#histogram")
+              .append("svg")
+              .append("g")
+              .attr("width", width)
+              .attr("height", height);
+
+  svg.append("g").attr("class", "histogram");
+    
+  function renderHistogram(data, i) {
+
+    var money_data = data.map( function(d) { return d.rounded_money; });
+
+    var x_range = [];
+    if (i === 0) {
+      x_range = [0, breakpoints[i]];
+    } else if (i === breakpoints.length) {
+      x_range = [breakpoints[i-1], 2000000];
+    } else {
+      x_range = [breakpoints[i-1], breakpoints[i]];
+    }
+
+    var x = d3.scale.linear()
+              .domain(x_range)
+              .range([0, width]);
+
+    var histogram_data = d3.layout.histogram()
+                           .bins(x.ticks(20))
+                           ( money_data );
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(histogram_data, function(d) { return d.y; })])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var bar = svg.selectAll(".bar")
+                 .data(histogram_data)
+                 .enter().append("g")
+                 .attr("class", "bar")
+                 .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
+                  
+                 //.data(histogram_data)
+                 //.enter().append("g")
+                 //.attr("class", "bar") 
+                 //.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
+
+    console.log(histogram_data)
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(breakpoints[i-1] + histogram_data[0].dx) - 1)
+        .attr("height", function(d) { return height - y(d.length); })
+        .attr("fill", "blue");
+
+    //bar.exit()
+    //   .remove();
+  }
 });
